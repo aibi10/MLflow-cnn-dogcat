6415421a523469a41bfc47fa51d45004cd9073eb
aibi10 <isingh.abhishek10@gmail,com>
2022-08-21 04:27:46 +0530

    get data

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("READ.md", "r", encoding = "utf-8") as f:
+with open("README.md", "r", encoding = "utf-8") as f:
     long_description = f.read()
 
 REPO_NAME = "mlflow-project-template"
@@ -15,7 +15,7 @@ setup(
     description = "a small description for MLFlow app",
     long_description = "long_description",
     long_description_content_type = "text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email = "isingh.abhishek10@gmail.com",
     packages = [SRC_REPO],
     License = "MIT",
--- src/stage__01__get__data.py
+++ src/stage__01__get__data.py
@@ -6,33 +6,32 @@ import logging
 from src.utils.common import read_yaml, create_directories
 import random
 
-STAGE = "STAGE_NAME"
+STAGE = "DATA LOAD"
 
 logging.basicConfig (
     filename = os.path.join("logs", "running_logs.log"),
     level = logging.INFO,
-    format = [%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode = "a"
 )
 
 
-def main(config_path, params_path):
+def main(config_path):
     config = read_yaml(config_path)
-    params = read_yaml(config_path)
+    local_dir = config['data']['local_dir']
+    create_directories(local_dir)                       # a folder is created with the name 'data'
+    data_file = config['data']['file_name']
+    data_file_path = os.path.join(local_dir, data_file)
 
-    pass
-
-
-if name == "__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default = "configs/config.yaml")
-    args.add_argument("--params", "-p", default = "params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n***************************")
         logging.info(f">>>>>>>>>>> stage   {STAGE}   started <<<<<<<<<<")
-        main(config_path = parsed_args.config, params_path = parsed_args.params)
+        main(config_path = parsed_args.config)
         logging.info(f">>>>>>>>> stage  {STAGE}   completed <<<<<<<<<<<")
     except Exception as e:
         logging.exception(e)
