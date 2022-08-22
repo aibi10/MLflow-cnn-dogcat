adfcde7f2d51b79e54b1164e5f3882c3093f9233
aibi10 <isingh.abhishek10@gmail,com>
2022-08-23 04:29:08 +0530

    integrating stage 01 and 02

--- src/stage__00__template.py
+++ src/stage__00__template.py
@@ -23,7 +23,7 @@ def main(config_path, params_path):
     pass
 
 
-if name == "__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default = "configs/config.yaml")
     args.add_argument("--params", "-p", default = "params.yaml")
